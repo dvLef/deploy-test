95f81fd6d6dbf4d68f30777a4c40473b68d45cae
Jonathan Jara Assmann <j.jara21@ufromail.cl>
2022-09-29 15:39:49 -0300

    Se actualiza titulo de la pagina

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Deploy test
                 </div>
 
                 <div class="links">
